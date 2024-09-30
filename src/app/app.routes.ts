f9d2c7b05f8666d75c81df581f4e2208dd001197
lela-akiashvili <l.akiashvili.doit@itstep.ge>
2024-09-30 16:57:30 +0400

    E-LAB #9: add routing for profile page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'profile-page',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./components/profile-page/profile-page.component').then(
+        (m) => m.ProfilePageComponent
+      ),
+  },
+];
