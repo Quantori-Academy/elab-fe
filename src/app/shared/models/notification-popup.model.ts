dc1fc6deb4033b87e4c1ba041a5db64b9d9a3ba3
Magadanov <77725865+Magadanov@users.noreply.github.com>
2024-10-10 17:43:10 +0500

    E-LAB FE #47: Refactor code Add position (#50)

--- src/app/shared/models/notification-popup.model.ts
+++ src/app/shared/models/notification-popup.model.ts
@@ -5,9 +5,16 @@ export enum NotificationType {
   WARNING = 'warning',
 }
 
+export type NotificationPosition =
+  | 'top-left'
+  | 'top-right'
+  | 'bottom-left'
+  | 'bottom-right';
+
 export interface NotificationData {
   type: NotificationType;
   title: string;
   message: string;
   duration?: number;
+  position?: NotificationPosition;
 }
