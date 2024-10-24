7d7ca6e179b2d0feef7d646cf79ebfffe70207cc
Denis <denis.mail.tech@gmail.com>
2024-10-24 14:59:12 +0300

    Updated

--- src/app/shared/services/error-handling/global-error-handling.component.ts
+++ src/app/shared/services/error-handling/global-error-handling.component.ts
@@ -8,38 +8,24 @@ export class GlobalErrorHandler implements ErrorHandler {
 
   handleError(error: unknown): void {
     if (error instanceof HttpErrorResponse) {
-      // You can adjust 401 for your component to handle it properly
-      if (error.status === 401) {
+      if (error.status === 0) {
         this.notificationPopupService.error({
-          title: 'Authorization error',
-          message: 'Access denied',
-          duration: 1000,
+          title: 'Failed to connect to the server',
+          message: 'Check your internet connection and try again',
+          duration: 3000,
         });
-      } else if (error.status === 0) {
-        if (!navigator.onLine) {
-          this.notificationPopupService.error({
-            title: 'Network Error',
-            message: 'No internet connection',
-            duration: 3000,
-          });
-        } else {
-          this.notificationPopupService.error({
-            title: 'CORS Error',
-            message: 'CORS Error',
-            duration: 3000,
-          });
-        }
       } else {
+        const serverMessage = error.error?.message || error.message;
         this.notificationPopupService.error({
-          title: 'Server Error',
-          message: 'Network Error',
+          title: `Error ${error.status}`,
+          message: serverMessage,
           duration: 3000,
         });
       }
     } else {
       this.notificationPopupService.error({
-        title: 'Unknown Error',
-        message: 'Unknown error',
+        title: 'Unexpected Error',
+        message: 'An unexpected error occurred',
         duration: 3000,
       });
     }
