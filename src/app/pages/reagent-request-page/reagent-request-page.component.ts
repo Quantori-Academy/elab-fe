9339b5606ddf1a5b7ed8173cd2034392ff865639
Denis <denis.mail.tech@gmail.com>
2024-10-29 10:43:38 +0300

    Basic layout

--- src/app/pages/reagent-request-page/reagent-request-page.component.ts
+++ src/app/pages/reagent-request-page/reagent-request-page.component.ts
@@ -1,21 +1,10 @@
-import {
-  AfterViewInit,
-  Component,
-  inject,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
-import { ReagentRequestService } from './reagent-request.service';
-import { ReagentRequest } from './reagent-request.interface';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
-import { Sort } from '@angular/material/sort';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
 import { MaterialModule } from '../../material.module';
 import { MoleculeStructureComponent } from '../../shared/components/molecule-structure/molecule-structure.component';
-import { StructureDialogComponent } from '../reagents-list/components/structure-dialog/structure-dialog.component';
 
 @Component({
   selector: 'app-reagents-request-page',
@@ -30,9 +19,9 @@ import { StructureDialogComponent } from '../reagents-list/components/structure-
   templateUrl: './reagent-request-page.component.html',
   styleUrls: ['./reagent-request-page.component.scss'],
 })
-export class ReagentsRequestPageComponent implements OnInit, AfterViewInit {
+export class ReagentsRequestPageComponent implements OnInit {
   public dialog = inject(MatDialog);
-  private reagentRequestService = inject(ReagentRequestService);
+  // private reagentRequestService = inject(ReagentRequestService);
 
   selectedStatus = '' as 'Pending' | 'Ordered' | 'Declined' | 'Fulfilled' | '';
   currentPage = 0;
@@ -52,98 +41,15 @@ export class ReagentsRequestPageComponent implements OnInit, AfterViewInit {
     'updatedAt',
   ];
 
-  dataSource = new MatTableDataSource<ReagentRequest>();
+  // dataSource = new MatTableDataSource<ReagentRequest>();
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   ngOnInit(): void {
-    this.loadReagentRequests(false);
-  }
-
-  loadReagentRequests(applySorting = true) {
-    console.log('Fetching reagent requests with filters:', {
-      status: this.selectedStatus,
-      sortByCreatedDate:
-        applySorting && this.sortColumn === 'createdAt'
-          ? this.sortDirection
-          : undefined,
-      sortByQuantity:
-        applySorting && this.sortColumn === 'quantity'
-          ? this.sortDirection
-          : undefined,
-      sortByUpdatedDate:
-        applySorting && this.sortColumn === 'updatedAt'
-          ? this.sortDirection
-          : undefined,
-      skip: this.currentPage * (this.paginator ? this.paginator.pageSize : 10),
-      take: this.paginator ? this.paginator.pageSize : 10,
-      name: this.filterName || undefined,
-    });
-
-    this.reagentRequestService
-      .getReagentRequests(
-        this.selectedStatus || undefined,
-        this.sortColumn === 'quantity' ? this.sortDirection : undefined,
-        this.sortColumn === 'createdAt' ? this.sortDirection : undefined,
-        this.sortColumn === 'updatedAt' ? this.sortDirection : undefined,
-        this.currentPage * (this.paginator ? this.paginator.pageSize : 10),
-        this.paginator ? this.paginator.pageSize : 10,
-        this.filterName || undefined
-      )
-      .subscribe({
-        next: (requests) => {
-          this.dataSource.data = requests;
-        },
-        error: (err) => {
-          console.error('Error:', err);
-        },
-      });
-  }
-
-  ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator;
-    this.paginator.page.subscribe((event) => {
-      this.currentPage = event.pageIndex;
-      this.loadReagentRequests();
-    });
-  }
-
-  applyFilter() {
-    this.currentPage = 0;
-    this.paginator.firstPage();
     this.loadReagentRequests();
   }
 
-  onSortChange(sort: Sort) {
-    if (['createdAt', 'quantity', 'updatedAt'].includes(sort.active)) {
-      this.sortColumn = sort.active as 'createdAt' | 'quantity' | 'updatedAt';
-      this.sortDirection = sort.direction === 'asc' ? 'asc' : 'desc';
-      this.loadReagentRequests();
-    } else {
-      this.sortColumn = 'createdAt';
-      this.sortDirection = 'asc';
-      this.loadReagentRequests();
-    }
+  loadReagentRequests() {
+    // in progress
   }
-
-  openStructure(structure: string) {
-    this.dialog.open(StructureDialogComponent, {
-      data: { structure },
-      panelClass: 'image-dialog',
-    });
-  }
-
-  // Currently in progress
-
-  // openCreateReagentRequestDialog() {
-  //   const dialogRef = this.dialog.open(NewReagentFormComponent, {
-  //     data: {},
-  //   });
-
-  //   dialogRef.afterClosed().subscribe((result) => {
-  //     if (result) {
-  //       this.loadReagentRequests();
-  //     }
-  //   });
-  // }
 }
