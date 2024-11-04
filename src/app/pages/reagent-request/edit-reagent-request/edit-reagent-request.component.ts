5b99c3490803edd7b52cc8af1256a6b7940d2b57
Denis <denis.mail.tech@gmail.com>
2024-11-04 18:15:44 +0300

    New files of edit

---
+++ src/app/pages/reagent-request/edit-reagent-request/edit-reagent-request.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-reagent-request',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-reagent-request.component.html',
+  styleUrl: './edit-reagent-request.component.scss'
+})
+export class EditReagentRequestComponent {
+
+}
