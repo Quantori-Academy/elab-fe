a0d8a2135270fba6edd73de63d0ae01c4b039185
Sophia Shashova <141850987+sonkashash@users.noreply.github.com>
2024-11-07 13:19:51 +0400

    Elab FE #92 Refinement of New Reagent Creation (#127)
    * Elab FE #92 Refinement of New Reagent Creation(add structure editor in create-reagent page)
    
    * added unsubscribe form data

--- src/app/shared/components/structure-editor/structure-editor.component.ts
+++ src/app/shared/components/structure-editor/structure-editor.component.ts
@@ -1,4 +1,6 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { TableLoaderSpinnerComponent } from "../table-loader-spinner/table-loader-spinner.component";
 
 interface Ketcher {
   getSmiles(): Promise<string>;
@@ -15,16 +17,36 @@ declare global {
   standalone: true,
   templateUrl: './structure-editor.component.html',
   styleUrls: ['./structure-editor.component.scss'],
+  imports: [TableLoaderSpinnerComponent],
 })
-export class StructureEditorComponent {
-  @ViewChild('ketcherFrame', { static: true })
+export class StructureEditorComponent implements OnInit, AfterViewInit {
+  @ViewChild('ketcherFrame', { static: false })
   ketcherFrame!: ElementRef<HTMLIFrameElement>;
 
+  ketcherUrl!: SafeResourceUrl;
+  loading = true;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit() {
+    this.ketcherUrl = this.sanitizer.bypassSecurityTrustResourceUrl('/ketcher/index.html');
+  }
+
+  ngAfterViewInit() {
+    if (this.ketcherFrame) {
+      this.ketcherFrame.nativeElement.onload = () => this.onLoaded();
+    }
+  }
+
   get ketcher(): Ketcher | null {
-    return this.ketcherFrame.nativeElement.contentWindow?.ketcher as Ketcher;
+    return this.ketcherFrame?.nativeElement.contentWindow?.ketcher || null;
   }
 
   public async getSmiles(): Promise<string> {
-    return await this.ketcher!.getSmiles();
+      return await this.ketcher!.getSmiles();
+  }
+
+  onLoaded() {
+    this.loading = false;
   }
 }
