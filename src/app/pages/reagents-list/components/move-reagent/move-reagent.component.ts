787ad59ea33f338d09385ea4c830f885a3ee4b2f
Magadanov <77725865+Magadanov@users.noreply.github.com>
2024-11-20 17:19:35 +0500

    ELAB FE #136: Move Reagents (#154)
    * E-LAB FE #136: Implement move reagents without search api
    
    * E-LAB FE #136: add storage autocomplete
    
    * E-LAB FE #136: refactor move reagent component
    
    * E-LAB FE #136: refactor move reagent component
    
    * E-LAB FE #136: refactor move reagent
    
    * E-LAB FE #136: set checkbox condition

---
+++ src/app/pages/reagents-list/components/move-reagent/move-reagent.component.ts
@@ -0,0 +1,127 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  Inject,
+  OnInit,
+} from '@angular/core';
+import { MaterialModule } from '../../../../material.module';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { StorageLocationService } from '../../../storage-location/services/storage-location.service';
+import { MoveReagentData } from '../../../../shared/models/reagent-model';
+import { firstValueFrom, Observable } from 'rxjs';
+import { NotificationPopupService } from '../../../../shared/services/notification-popup/notification-popup.service';
+import {
+  StorageLocationItem,
+  StorageLocationListData,
+} from '../../../storage-location/models/storage-location.interface';
+import { StorageLocationQueryService } from '../../../storage-location/services/storage-location-query.service';
+import { StorageLocationColumn } from '../../../storage-location/models/storage-location.enum';
+import { AsyncPipe } from '@angular/common';
+import { storageLocationAutoCompleteValidator } from '../../../../shared/validators/storage-location-autocomplete.validator';
+
+@Component({
+  selector: 'app-move-reagent',
+  standalone: true,
+  imports: [MaterialModule, FormsModule, AsyncPipe, ReactiveFormsModule],
+  providers: [StorageLocationService, StorageLocationQueryService],
+  templateUrl: './move-reagent.component.html',
+  styleUrl: './move-reagent.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MoveReagentComponent implements OnInit {
+  private storageLocationService = inject(StorageLocationService);
+  private storageLocationQueryService = inject(StorageLocationQueryService);
+  private notificationPopupService = inject(NotificationPopupService);
+  private dialogRef = inject(MatDialogRef<MoveReagentComponent>);
+  private fb = inject(FormBuilder);
+
+  public movedReagents!: Map<number, Set<number>>;
+  public storageLocations$?: Observable<StorageLocationListData>;
+  public moveForm = this.fb.group({
+    storageLocation: [
+      '',
+      [Validators.required, storageLocationAutoCompleteValidator()],
+    ],
+    destinationStorageId: [null as number | null, Validators.required],
+  });
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: { movedReagents: Map<number, Set<number>> }
+  ) {
+    this.movedReagents = this.data.movedReagents;
+  }
+
+  ngOnInit(): void {
+    this.storageLocations$ = this.storageLocationService.searchStorageLocationByName();
+  }
+
+  displayFn = (option: StorageLocationItem): string => {
+    this.moveForm.get('destinationStorageId')?.setValue(option.id);
+    return option ? `${option.room.name} ${option.name}` : '';
+  };
+
+  onRoomNameChange($event: Event) {
+    const value = ($event.target as HTMLInputElement).value;
+    this.storageLocationQueryService.nameFilterSubject.next({
+      value,
+      column: StorageLocationColumn.FullPath,
+    });
+  }
+
+  async moveReagents() {
+    if (this.moveForm.valid) {
+      const destinationStorageId = this.moveForm.value.destinationStorageId!;
+
+      const movePromises = Array.from(this.movedReagents.entries()).map(
+        ([sourceStorageId, reagentSet]) => {
+          const reagents = Array.from(reagentSet).map((id) => ({ id }));
+          const data: MoveReagentData = {
+            sourceStorageId,
+            destinationStorageId,
+            reagents,
+          };
+
+          return firstValueFrom(
+            this.storageLocationService.moveReagentStorageLocation(data)
+          );
+        }
+      );
+
+      const httpResponses = await Promise.allSettled(movePromises);
+
+      const rejectedRes = httpResponses.filter((res) => res.status === 'rejected');
+
+      if (httpResponses.length === rejectedRes.length) {
+        this.notificationPopupService.error({
+          title: 'Error',
+          message: "Reagents couldn't move to storage",
+        });
+        this.dialogRef.close(false);
+      } else {
+        if (!rejectedRes.length) {
+          this.notificationPopupService.success({
+            title: 'Success',
+            message: 'Reagents successfully moved',
+          });
+        } else {
+          this.notificationPopupService.warning({
+            title: 'Warning',
+            message: rejectedRes
+              .map((res) => res.reason.error.message)
+              .join('\n'),
+          });
+        }
+        this.movedReagents.clear();
+        this.dialogRef.close(true);
+      }
+    }
+  }
+}
--- src/app/pages/reagents-list/reagents-list.component.ts
+++ src/app/pages/reagents-list/reagents-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, computed, inject, Input, OnInit } from '@angular/core';
 import { ReagentsService } from '../../shared/services/reagents.service';
 import {
+  Reagent,
   ReagentListColumn,
   ReagentListResponse,
 } from '../../shared/models/reagent-model';
 import { PageEvent } from '@angular/material/paginator';
 import { Sort } from '@angular/material/sort';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
 import { StructureDialogComponent } from './components/structure-dialog/structure-dialog.component';
@@ -14,11 +20,12 @@ import { MaterialModule } from '../../material.module';
 import { MoleculeStructureComponent } from '../../shared/components/molecule-structure/molecule-structure.component';
 import { Router } from '@angular/router';
 import { TableLoaderSpinnerComponent } from '../../shared/components/table-loader-spinner/table-loader-spinner.component';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import { ReagentsQueryService } from './services/reagents-query.service';
 import { PAGE_SIZE_OPTIONS } from '../../shared/units/variables.units';
 import { SpinnerDirective } from '../../shared/directives/spinner/spinner.directive';
 import { RbacService } from '../../auth/services/authentication/rbac.service';
+import { MoveReagentComponent } from './components/move-reagent/move-reagent.component';
 
 @Component({
   selector: 'app-reagents-list',
@@ -43,14 +50,22 @@ export class ReagentsListComponent implements OnInit {
   private reagentsQueryService = inject(ReagentsQueryService);
   private rbacService = inject(RbacService);
   private router = inject(Router);
+  private fb = inject(FormBuilder);
 
   public currentPage = 0;
   public pageSize = this.reagentsQueryService.pageSize;
   public isLoading = computed(() => this.reagentsQueryService.isLoading());
   public pageSizeOptions = inject(PAGE_SIZE_OPTIONS);
   public isResearcher = this.rbacService.isResearcher();
+  public movedReagents = new Map<number, Set<number>>();
+  public isOpenMoveForm = false;
+  public moveForm = this.fb.group({
+    sourceStorageId: ['', [Validators.required]],
+    destinationStorageId: ['', [Validators.required]],
+  });
 
   public displayedColumns: ReagentListColumn[] = [
+    ...(this.isResearcher ? [ReagentListColumn.CHECKBOX] : []),
     ReagentListColumn.NAME,
     ReagentListColumn.CATEGORY,
     ReagentListColumn.STRUCTURE,
@@ -67,6 +82,44 @@ export class ReagentsListComponent implements OnInit {
     this.reagentsResponse$ = this.storageLocationId
       ? this.reagentsService.getReagents(this.storageLocationId)
       : this.reagentsService.getReagents();
+  }
+
+  isReagentsSelected(): boolean {
+    return this.movedReagents.size !== 0;
+  }
+
+  selectMovedReagents(element: Reagent) {
+    if (this.movedReagents.has(element.storageId)) {
+      const movedReagentStorage = this.movedReagents.get(element.storageId);
+      if (movedReagentStorage?.has(element.id!)) {
+        movedReagentStorage.delete(element.id!);
+      } else {
+        movedReagentStorage?.add(element.id!);
+      }
+
+      if (!movedReagentStorage?.size) {
+        this.movedReagents.delete(element.storageId);
+      }
+    } else {
+      this.movedReagents.set(element.storageId, new Set([element.id!]));
+    }
+  }
+
+  openMoveReagentDialog() {
+    if (this.movedReagents.size) {
+      this.dialog
+        .open(MoveReagentComponent, {
+          data: { movedReagents: this.movedReagents },
+          minWidth: '400px',
+        })
+        .afterClosed()
+        .pipe(take(1))
+        .subscribe((value) => {
+          if (value) {
+            this.reagentsQueryService.reloadReagentList();
+          }
+        });
+    }
   }
 
   onFilterName($event: Event) {
