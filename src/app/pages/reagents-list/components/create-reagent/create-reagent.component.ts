988f222e676c0971afb3cd045b3b6f11ed0dc0ba
Magadanov <magadanov03@mail.ru>
2024-12-05 17:32:04 +0500

    E-LAB FE #202:  Populate "quantityLeft" field silently based on "totalQuantity"

--- src/app/pages/reagents-list/components/create-reagent/create-reagent.component.ts
+++ src/app/pages/reagents-list/components/create-reagent/create-reagent.component.ts
@@ -110,7 +110,6 @@ export class CreateReagentComponent implements OnInit, OnDestroy {
       structure: [null],
       quantityUnit: ['', Validators.required],
       totalQuantity: [null, Validators.required],
-      quantityLeft: [null, Validators.required],
       storageLocation: [
         '',
         [Validators.required, storageLocationAutoCompleteValidator()],
@@ -118,9 +117,10 @@ export class CreateReagentComponent implements OnInit, OnDestroy {
       storageId: [null as number | null, Validators.required],
       ...(this.isSample
         ? {
-            usedReagentSample: [[]],
-          }
+          usedReagentSample: [[]],
+        }
         : {
+            quantityLeft: [null, Validators.required],
             casNumber: [
               null,
               [Validators.minLength(5), Validators.maxLength(10)],
@@ -231,7 +231,9 @@ export class CreateReagentComponent implements OnInit, OnDestroy {
           expirationDate: finalExpirationDate,
         };
       }
-      console.log(formRawValue);
+      if (this.isSample) {
+        formRawValue.quantityLeft = formRawValue.totalQuantity
+      }
 
       const formRequest = this.isSample
         ? this.reagentsService.createSample(formRawValue)
