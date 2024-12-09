41eb3e0799c94f1187fdadd6086af3ccc6401211
lela-akiashvili <l_akiashvili@cu.edu.ge>
2024-12-09 17:34:58 +0400

    ELAB-FE #207: Add translation to history dialog and fix dialog display

--- src/app/pages/reagents-list/components/reagent-history-dialog/reagent-history-dialog.component.ts
+++ src/app/pages/reagents-list/components/reagent-history-dialog/reagent-history-dialog.component.ts
@@ -6,20 +6,28 @@ import {
 } from '../../../../shared/models/reagent-model';
 import { ReagentsService } from '../../../../shared/services/reagents.service';
 import { MaterialModule } from '../../../../material.module';
-import { AsyncPipe, DatePipe } from '@angular/common';
+import { AsyncPipe, DatePipe, UpperCasePipe } from '@angular/common';
 import { BehaviorSubject, map } from 'rxjs';
 import { TableLoaderSpinnerComponent } from '../../../../shared/components/table-loader-spinner/table-loader-spinner.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-reagent-history-dialog',
   standalone: true,
-  imports: [MaterialModule, DatePipe, AsyncPipe, TableLoaderSpinnerComponent],
+  imports: [
+    MaterialModule,
+    DatePipe,
+    AsyncPipe,
+    TableLoaderSpinnerComponent,
+    TranslateModule,
+    UpperCasePipe,
+  ],
   templateUrl: './reagent-history-dialog.component.html',
   styleUrl: './reagent-history-dialog.component.scss',
 })
 export class ReagentHistoryDialogComponent implements OnInit {
   reagentService = inject(ReagentsService);
-  reagentHistory$ = new BehaviorSubject<ReagentHistory[] | null>(null); // Initialize with null
+  reagentHistory$ = new BehaviorSubject<ReagentHistory[] | null>(null);
 
   public displayedColumns = [
     'action',
@@ -33,19 +41,21 @@ export class ReagentHistoryDialogComponent implements OnInit {
 
   constructor(
     public dialogRef: MatDialogRef<ReagentHistoryDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Reagent,
+    @Inject(MAT_DIALOG_DATA) public data: Reagent
   ) {}
 
   ngOnInit(): void {
-    // Fetch history data
-    this.reagentService.getReagentsHistory().pipe(
-      map((resp) =>
-        resp.filter(
-          (reagent) =>
-            reagent.oldData?.id === this.data.id ||
-            reagent.newData?.id === this.data.id
+    this.reagentService
+      .getReagentsHistory()
+      .pipe(
+        map((resp) =>
+          resp.filter(
+            (reagent) =>
+              reagent.oldData?.id === this.data.id ||
+              reagent.newData?.id === this.data.id
+          )
         )
       )
-    ).subscribe((filteredData) => this.reagentHistory$.next(filteredData)); // Push filtered data to the BehaviorSubject
+      .subscribe((filteredData) => this.reagentHistory$.next(filteredData));
   }
 }
